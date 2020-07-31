05ec72b1da6644075fed16e49c3debfee63a6553
Damoon Rashidi <damoon.rashidi@gmail.com>
2020-07-31 13:56:43 +0200

    update some deps

---
+++ src/lines/colors.ts
@@ -0,0 +1,11 @@
+import { random } from './helpers';
+export default {
+  bg: '#fffeee',
+  main: '#b54c38',
+};
+
+export const randomHue = (
+  min: number = 0,
+  max: number = 360,
+  opacity = 1
+): string => `hsla(${random(min, max)}, 70%, 60%, ${opacity})`;
---
+++ src/lines/index.ts
@@ -0,0 +1,47 @@
+import { init, random, Line } from './helpers';
+import { randomHue } from './colors';
+const WIDTH = 3000;
+const HEIGHT = 4000;
+
+const paint = (ctx: CanvasRenderingContext2D) => {
+  const lines: Line[] = Array(300)
+    .fill(0)
+    .map(() => {
+      const height = random(20, HEIGHT / 2);
+
+      return {
+        x: random(0, WIDTH),
+        y: random(-HEIGHT, HEIGHT),
+        color: randomHue(),
+        width: random(2, 30),
+        height,
+      };
+    });
+
+  lines.forEach(({ x, y, width, height, color }) => {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.ellipse(x + width / 2, y, width / 2, width / 2, 0, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.ellipse(
+      x + width / 2,
+      y + height,
+      width / 2,
+      width / 2,
+      0,
+      0,
+      Math.PI * 2
+    );
+    ctx.fill();
+    ctx.fillRect(x, y, width, height);
+    ctx.closePath();
+  });
+};
+
+setTimeout(() => {
+  const ctx = init(WIDTH, HEIGHT);
+  ctx.fillStyle = '#061729';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  paint(ctx);
+});
