7dc4e295675d2a6ecdde3fe672ea7d9c3717141b
Damoon Rashidi <damoon.rashidi@gmail.com>
2020-12-24 20:55:48 +0100

    Update winter

--- src/winter/index.ts
+++ src/winter/index.ts
@@ -1,39 +1,90 @@
-import { init, random, middle, pointAlong, between } from '../lib';
-const WIDTH = 2890;
+import {
+  init,
+  random,
+  middle,
+  pointAlong,
+  between,
+  Vector2D,
+  paintGrid,
+} from '../lib';
+const WIDTH = 2160;
 const HEIGHT = 3860;
-type Vector2D = [number, number];
 
-/**
- * The X and Y here are the starting posistion for the crown of the Tree
- */
-const tree = ([x, y]: Vector2D) => {};
+function makeTree([startX, startY]: Vector2D, treeHeight: number): Vector2D[] {
+  const trunkWidth: number = 50;
+  const branches: Vector2D[][] = [];
+  const tree: Vector2D[] = Array(1200)
+    .fill(false)
+    .map(() => {
+      const y = random(startY, startY + treeHeight);
+      const topRatio = 1 - y / startY;
+      const minX = startX - topRatio * trunkWidth;
+      const maxX = startX + topRatio * trunkWidth;
+      const x = random(minX, maxX);
+      return [x, y];
+    });
+
+  for (
+    let branchY = startY;
+    branchY < startY + treeHeight;
+    branchY += treeHeight / 16
+  ) {
+    for (let b = 0; b < 1000; b++) {
+      const topRatio = 1 - branchY / startY;
+      const branchWidth = (topRatio * branchY) / 4;
+      const minX = startX - topRatio * branchWidth;
+      const maxX = startX + topRatio * branchWidth;
+      const y = random(branchY - 10, branchY + 10);
+      const x = random(minX, maxX);
+      const point: Vector2D = [x, y];
+      tree.push(point);
+    }
+  }
+
+  return tree;
+}
+
+function paintTree(ctx: CanvasRenderingContext2D, tree: Vector2D[]) {
+  tree.forEach(([x, y]) => {
+    ctx.beginPath();
+    ctx.arc(x, y, random(0.5, 1.5, false), 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fillStyle = '#222';
+    ctx.fill();
+  });
+}
+
+function createSnow(ctx: CanvasRenderingContext2D): void {
+  const w = random(200, 800);
+  const h = random(200, 500);
+  const x = random(0, WIDTH - w);
+  const y = random(3088, HEIGHT);
+
+  ctx.save();
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + w, y);
+  ctx.lineTo(x + w, y + h);
+  ctx.lineTo(x, y);
+  ctx.closePath();
+  ctx.fillStyle = 'rgba(255,255,255,0.4)';
+  ctx.filter = `blur(128px)`;
+  ctx.fill();
+  ctx.restore();
+}
 
 const paint = (ctx: CanvasRenderingContext2D) => {
-  /**
-   * this is assuming gaussian distrubtion for dots, but we can probably do better
-   * if we segment the tree at this point and the do branches and trunk at the same time.. or maybe
-   * its a nicer effect to do the trunk first, and then branches
-   */
+  // paintGrid(ctx, WIDTH, HEIGHT, { showNumbers: true });
 
-  for (let a = 0; a < 30; a++) {
-    const adjustX = random(-50, 500);
-    const adjustY = random(-500, 500);
-    for (let i = 0; i < 1000; i++) {
-      const trunk = [
-        WIDTH / 2 - adjustX,
-        HEIGHT / 2 + adjustY,
-        WIDTH / 2 + adjustX + 10,
-        HEIGHT / 3 + adjustY,
-      ];
-      const x = random(trunk[0], trunk[2], false);
-      const y = random(trunk[1], trunk[3], false);
+  for (let i = 0; i < 50; i++) {
+    const [x, y] = [random(0, WIDTH), random(HEIGHT - 1300, HEIGHT - 1100)];
 
-      ctx.beginPath();
-      ctx.fillStyle = '#555';
-      ctx.arc(x, y, 1, 0, Math.PI * 2);
-      ctx.fill();
-      ctx.closePath();
-    }
+    const tree = makeTree([x, y], 1000);
+    paintTree(ctx, tree);
+  }
+
+  for (let i = 0; i < 60; i++) {
+    createSnow(ctx);
   }
 };
 
@@ -57,7 +108,7 @@ setTimeout(() => {
  *   * Branch distortion
  *   * unevenness
  *
- *           ^
+ *           ^ <------ [x, y]
  *          ~|~
  *       ~~~~|~~~~
  *        ~~~|~~~
